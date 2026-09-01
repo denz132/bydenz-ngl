3f24efb9e283326c8080c959dccd7b9ea0ef5e80
denz132 <dhanztzy5@gmail.com>
2026-09-01 19:18:06 +0700

    add cloudflare worker

---
+++ worker/index.js
@@ -0,0 +1,9 @@
+export default {
+  async fetch(request, env) {
+    return new Response("BY DENZ NGL ONLINE 🔥", {
+      headers: {
+        "content-type": "text/plain; charset=UTF-8"
+      }
+    });
+  }
+};
