cc25439240e46ca75b35d253b005ce96cccd58bd
denz132 <dhanztzy5@gmail.com>
2026-09-18 13:42:54 +0700

    Replace static response with asset fetch
    Memperbaiki

--- worker/index.js
+++ worker/index.js
@@ -1,9 +1,5 @@
 export default {
   async fetch(request, env) {
-    return new Response("BY DENZ NGL ONLINE 🔥", {
-      headers: {
-        "content-type": "text/plain; charset=UTF-8"
-      }
-    });
+    return env.ASSETS.fetch(request);
   }
 };
